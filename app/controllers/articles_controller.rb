5941c3c9ac1c6563dd276f7fe49b0cb8d2ecc3a8
NikitaTKachenkooooo <101145632+NikitaTKachenkooooo@users.noreply.github.com>
2023-12-05 00:17:12 +0300

    new change

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
   def index
     @articles = Article.all
   end
@@ -53,4 +54,11 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :body)
     end
+
+    
+  private
+    def article_params
+      params.require(:article).permit(:title, :body, :status)
+    end
+
 end
